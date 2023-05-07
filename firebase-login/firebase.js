ce24913f8fbf356629df4deeeffffcc11102545d
lblake <treleven.lloyd@gmail.com>
2023-05-07 23:15:19 +0100

    updated firebase file to use firebase 9 setup

--- firebase-login/firebase.js
+++ firebase-login/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from 'firebase';
+// import firebase from 'firebase/app';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,13 +16,16 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
-if (firebase.apps.length === 0) {
-  app = firebase.initializeApp(firebaseConfig);
-} else {
-  app = firebase.app();
-}
+const app = initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
+export const auth = getAuth(app);
+// let app;
+// if (firebase.apps.length === 0) {
+//   app = firebase.initializeApp(firebaseConfig);
+// } else {
+//   app = firebase.app();
+// }
 
-export { auth };
+// const auth = firebase.auth();
+
+// export { auth };
